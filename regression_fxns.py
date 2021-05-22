dc4ee412753b5937f8ee0e81ce19cd790d537da1
Kei Horikawa <horikkei@oregonstate.edu>
2021-05-22 06:25:36 +0000

    made first and second functions

--- regression_fxns.py
+++ regression_fxns.py
@@ -10,12 +10,16 @@ def make_standard_units(input_array):
     '''Converts input_array to standard_units, where data has mean 0 and standard deviation of 1
         INPUT: data array
         OUTPUT: array in standard units'''
-
+    mean_subtracted_array=input_array-np.mean(input_array)
+    normalized_array=mean_subtracted_array/np.std(input_array)
+    return normalized_array
     
 def calc_corrcoef_from_standardized_input(array1,array2):
     '''Calculates Pearson correlation coefficient from two arrays in standard units
     INPUT: array1, array2: In standard units
     OUTPUT: Pearson correlation coefficient'''
+    corr=np.mean(array1*array2)
+    return corr
 
 def get_regression_parameters(array1, array2):
     '''Calculates regression parameteres from two input arrays
